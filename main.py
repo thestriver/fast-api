b5178549f51ebdb439b6689b0a158821073b0510
thestriver <wannabeamumin@gmail.com>
2021-07-22 19:14:31 -0600

    fast_api

---
+++ main.py
@@ -0,0 +1,46 @@
+# main.py
+
+from typing import Optional
+
+from fastapi import FastAPI
+
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+app = FastAPI()
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+# @app.put("/items/{item_id}")
+# async def create_item(item_id: int, item: Item):
+#     return {"item_id": item_id, **item.dict()}
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Hello World"}
+
+# # put fixed paths first
+# @app.get("/users/me")
+# async def read_user_me():
+#     return {"user_id": "the current user"}
+
+# @app.get("/users/{user_id}")
+# async def read_user(user_id: str):
+#     return {"user_id": user_id}
+
+
+
+# uvicorn main:app --reload
+    
